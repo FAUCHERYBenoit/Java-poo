d84c6428c332f18c48537b28e2f15a4ae6d77c95
benoit.fauchery <Benoit.Fauchery@cgi.com>
2023-04-12 15:28:27 +0200

    Exercices 3-5

--- src/main/java/com/ipi/javapoo/ExercicesNotes.java
+++ src/main/java/com/ipi/javapoo/ExercicesNotes.java
@@ -1,6 +1,12 @@
 package com.ipi.javapoo;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class ExercicesNotes {
+	
+	Integer numberEx5;
+	Random random = new Random();
 	
 	public void ex1(String chaine){
 		StringBuilder returnChaine = new StringBuilder();
@@ -11,7 +17,7 @@ public class ExercicesNotes {
 		markSeparation();
 	}
 	
-	public void ex2(int hauteur){
+	public void ex2(Integer hauteur){
 		int k = 0;
 		
 		for (int i = 1; i <= hauteur; ++i, k = 0) {
@@ -27,6 +33,85 @@ public class ExercicesNotes {
 			System.out.println();
 		}
 		markSeparation();
+	}
+	
+	public void ex3(){
+		int number = 0;
+		for (int i = 0;i<10000;i++){
+			number += i;
+		}
+		System.out.println(number);
+		markSeparation();
+	}
+	
+	public void ex4(Integer number){
+		if(number % 2 == 0){
+			System.out.print("Le nombre est pair");
+		}
+		else{
+			System.out.print("Le nombre est impair");
+		}
+		
+		for (int i = 2; i <= number / 2; ++i) {
+			// non premier
+			if (number % i == 0) {
+				System.out.println(" et non premier");
+				return;
+			}
+		}
+		System.out.println(" et premier");
+		markSeparation();
+	}
+	
+	public void ex5(){
+		numberEx5 = random.nextInt(1000);
+		System.out.println("Jeu du juste nombre");
+		startGame();
+	}
+	
+	private void startGame() {
+		boolean isPlaying = true;
+		int nombreDeCoups = 0;
+		
+		while (isPlaying){
+			Scanner myObj = new Scanner(System.in);
+			System.out.println("Entrer un nombre :");
+			String stringNumber = myObj.nextLine();
+			try {
+				int number = Integer.parseInt(stringNumber);
+			
+				if(nombreDeCoups < 7){
+					nombreDeCoups++;
+					if(number > numberEx5){
+						System.out.println("Plus petit");
+					}
+					else if (number < numberEx5) {
+						System.out.println("Plus grand");
+					}
+					else{
+						System.out.println("Gagné!");
+						isPlaying = false;
+					}
+				}
+				else{
+					System.out.println("Perdu!");
+					isPlaying = false;
+				}
+			}
+			catch (NumberFormatException e){
+				System.out.println("Mauvaise saisie");
+			}
+		}
+
+		Scanner myObj = new Scanner(System.in);
+		System.out.println("Voulez-vous recommencer? (Y/N)");
+		if(myObj.nextLine().equals("y")){
+			markSeparation();
+			ex5();
+		}
+		else{
+			markSeparation();
+		}
 	}
 	
 	private void markSeparation() {
--- src/main/java/com/ipi/javapoo/JavaPooApplication.java
+++ src/main/java/com/ipi/javapoo/JavaPooApplication.java
@@ -2,17 +2,39 @@ package com.ipi.javapoo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class JavaPooApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JavaPooApplication.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(JavaPooApplication.class, args);
 		System.out.println("##########################################\n");
 		
 		ExercicesNotes exercicesNotes = new ExercicesNotes();
+		
 		exercicesNotes.ex1("Benoît FAUCHERY");
+		
 		exercicesNotes.ex2(10);
+		
+		exercicesNotes.ex3();
+		
+		Scanner myObj = new Scanner(System.in);
+		System.out.println("Entrer un nombre :");
+		String number = myObj.nextLine();
+		try{
+			exercicesNotes.ex4(Integer.parseInt(number));
+		}
+		catch (NumberFormatException e){
+			System.out.println("Mauvaise saisie");
+			System.out.println("\n");
+		}
+		
+		exercicesNotes.ex5();
+		
+		ctx.close();
 	}
 
 }
