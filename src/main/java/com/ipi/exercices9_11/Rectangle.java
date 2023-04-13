985d77bdd6a0dd891a0ffec8a6ca9b909ca3a519
benoit.fauchery <Benoit.Fauchery@cgi.com>
2023-04-13 11:58:49 +0200

    Exercices 9-10

--- src/main/java/com/ipi/exercices9_11/Rectangle.java
+++ src/main/java/com/ipi/exercices9_11/Rectangle.java
@@ -1,5 +1,22 @@
 package com.ipi.exercices9_11;
 
 public class Rectangle {
-
+	
+	private int height, width;
+	
+	public Rectangle(int height, int width) {
+		this.height = height;
+		this.width = width;
+	}
+	
+	public void display(){
+		for (int i = 0;i<height;i++){
+			System.out.print("#  ".repeat(width));
+			System.out.println();
+		}
+	}
+	
+	public void calculDeSurface(){
+		System.out.println(height*width + " cm²");
+	}
 }
---
+++ src/main/java/com/ipi/exercices9_11/Square.java
@@ -0,0 +1,9 @@
+package com.ipi.exercices9_11;
+
+public class Square extends Rectangle{
+	
+	public Square(int height, int width) {
+		super(height, width);
+	}
+	
+}
